3e368b26a9f8441c6b37212d9c46e3e32f8ed512
Igor Maznitsa <rrg4400@gmail.com>
2015-08-27 09:32:38 +0300

    added import/export text to the text editor

--- src/main/java/com/igormaznitsa/nbmindmap/utils/PlainTextEditor.java
+++ src/main/java/com/igormaznitsa/nbmindmap/utils/PlainTextEditor.java
@@ -15,16 +15,20 @@
  */
 package com.igormaznitsa.nbmindmap.utils;
 
-import com.igormaznitsa.nbmindmap.utils.Logger;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.io.File;
+import java.util.Locale;
 import javax.swing.JEditorPane;
 import javax.swing.JScrollPane;
+import javax.swing.filechooser.FileFilter;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.EditorKit;
+import org.apache.commons.io.FileUtils;
 import org.netbeans.editor.BaseDocument;
 import org.netbeans.editor.Utilities;
+import org.openide.filesystems.FileChooserBuilder;
 import org.openide.text.CloneableEditorSupport;
 import org.openide.text.NbDocument;
 
@@ -34,6 +38,19 @@ public class PlainTextEditor extends javax.swing.JPanel {
 
   private final BaseDocument doc;
 
+  private static final FileFilter TEXT_FILE_FILTER = new FileFilter() {
+
+    @Override
+    public boolean accept(final File f) {
+      return f.isDirectory() || f.getName().toLowerCase(Locale.ENGLISH).endsWith(".txt");
+    }
+
+    @Override
+    public String getDescription() {
+      return "Text files (*,txt)";
+    }
+  };
+  
   public PlainTextEditor(final String text) {
     initComponents();
 
@@ -43,13 +60,8 @@ public class PlainTextEditor extends javax.swing.JPanel {
 
     this.doc = Utilities.getDocument(editor);
 
-    try {
-      this.doc.replace(0, 0, text, null);
-    }
-    catch (BadLocationException ex) {
-      Logger.error("Can't set text", ex);
-    }
-
+    setText(text);
+    
     final Component compo;
 
     if (doc instanceof NbDocument.CustomEditor) {
@@ -60,7 +72,7 @@ public class PlainTextEditor extends javax.swing.JPanel {
       compo = new JScrollPane(editor);
     }
 
-    compo.setPreferredSize(new Dimension(512, 300));
+    compo.setPreferredSize(new Dimension(620, 440));
     
     this.add(compo, BorderLayout.CENTER);
   }
@@ -75,6 +87,16 @@ public class PlainTextEditor extends javax.swing.JPanel {
     }
   }
 
+  private void setText(final String text){
+    try {
+      this.doc.replace(0, 0, text, null);
+    }
+    catch (BadLocationException ex) {
+      Logger.error("Can't set text", ex);
+      throw new RuntimeException(ex);
+    }    
+  }
+  
   /**
    * This method is called from within the constructor to initialize the form.
    * WARNING: Do NOT modify this code. The content of this method is always
@@ -85,12 +107,85 @@ public class PlainTextEditor extends javax.swing.JPanel {
   private void initComponents() {
 
     editorPopup = new javax.swing.JPopupMenu();
+    jToolBar1 = new javax.swing.JToolBar();
+    buttonLoad = new javax.swing.JButton();
+    buttonSave = new javax.swing.JButton();
 
     setLayout(new java.awt.BorderLayout());
+
+    jToolBar1.setFloatable(false);
+    jToolBar1.setRollover(true);
+
+    buttonLoad.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/igormaznitsa/nbmindmap/icons/disk16.png"))); // NOI18N
+    buttonLoad.setToolTipText(org.openide.util.NbBundle.getMessage(PlainTextEditor.class, "PlainTextEditor.buttonLoad.toolTipText")); // NOI18N
+    buttonLoad.setFocusable(false);
+    buttonLoad.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+    buttonLoad.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+    buttonLoad.addActionListener(new java.awt.event.ActionListener() {
+      public void actionPerformed(java.awt.event.ActionEvent evt) {
+        buttonLoadActionPerformed(evt);
+      }
+    });
+    jToolBar1.add(buttonLoad);
+
+    buttonSave.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/igormaznitsa/nbmindmap/icons/file_save16.png"))); // NOI18N
+    buttonSave.setToolTipText(org.openide.util.NbBundle.getMessage(PlainTextEditor.class, "PlainTextEditor.buttonSave.toolTipText")); // NOI18N
+    buttonSave.setFocusable(false);
+    buttonSave.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+    buttonSave.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+    buttonSave.addActionListener(new java.awt.event.ActionListener() {
+      public void actionPerformed(java.awt.event.ActionEvent evt) {
+        buttonSaveActionPerformed(evt);
+      }
+    });
+    jToolBar1.add(buttonSave);
+
+    add(jToolBar1, java.awt.BorderLayout.PAGE_START);
   }// </editor-fold>//GEN-END:initComponents
+
+  private void buttonLoadActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonLoadActionPerformed
+    final File home = new File(System.getProperty("user.home"));
+    final File toOpen = new FileChooserBuilder("user-home-dir").setTitle("Open UTF8 encoded text file").
+            addFileFilter(TEXT_FILE_FILTER).setFileFilter(TEXT_FILE_FILTER).
+            setFilesOnly(true).
+            setDefaultWorkingDirectory(home).
+            showOpenDialog();
+    if (toOpen!=null){
+      try {
+        final String text = FileUtils.readFileToString(toOpen, "UTF-8");
+        setText(text);
+      }
+      catch (Exception ex) {
+        Logger.error("Error during text file loading", ex);
+        NbUtils.msgError("Can't read text file for error!");
+      }
+    }
+  }//GEN-LAST:event_buttonLoadActionPerformed
+
+  private void buttonSaveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonSaveActionPerformed
+    final File home = new File(System.getProperty("user.home"));
+    final File toSave = new FileChooserBuilder("user-home-dir").setTitle("Open UTF8 encoded text file").
+            addFileFilter(TEXT_FILE_FILTER).setFileFilter(TEXT_FILE_FILTER).
+            setFilesOnly(true).
+            setDefaultWorkingDirectory(home).
+            showSaveDialog();
+    if (toSave != null) {
+      try {
+        final String text = getText();
+        FileUtils.writeStringToFile(toSave, text, "UTF-8");
+      }
+      catch (Exception ex) {
+        Logger.error("Error during text file saving", ex);
+        NbUtils.msgError("Can't save text file for error!");
+      }
+    }
+  }//GEN-LAST:event_buttonSaveActionPerformed
 
 
   // Variables declaration - do not modify//GEN-BEGIN:variables
+  private javax.swing.JButton buttonLoad;
+  private javax.swing.JButton buttonSave;
   private javax.swing.JPopupMenu editorPopup;
+  private javax.swing.JToolBar jToolBar1;
   // End of variables declaration//GEN-END:variables
 }
