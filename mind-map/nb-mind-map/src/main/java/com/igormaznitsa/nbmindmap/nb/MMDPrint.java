32d0368504ba3fd0a4c8af8d40a34e9ee54f4c92
Igor Maznitsa <rrg4400@gmail.com>
2015-09-10 15:32:59 +0300

    improved print subsystem to be more intelligent

--- mind-map/nb-mind-map/src/main/java/com/igormaznitsa/nbmindmap/nb/MMDPrint.java
+++ mind-map/nb-mind-map/src/main/java/com/igormaznitsa/nbmindmap/nb/MMDPrint.java
@@ -32,6 +32,9 @@ public class MMDPrint {
 
   private final PrintPage[][] pages;
 
+  private static final double SMALL_SCALING_THRESHOLD = 0.20d;
+  private static final double SCALE_RATIO_THRESHOLD = 0.20d;
+
   public MMDPrint(final MindMapPanel panel, final int paperWidthInPixels, final int paperHeightInPixels, final double pageZoomFactor) {
     logger.info(String.format("Request to prepare print pages for %dx%d with scale %f", paperWidthInPixels, paperHeightInPixels, pageZoomFactor));
 
@@ -52,6 +55,11 @@ public class MMDPrint {
       cfg.setOtherLevelTextColor(Color.black);
       cfg.setCollapsatorBorderColor(Color.black);
       cfg.setCollapsatorBackgroundColor(Color.white);
+
+      cfg.setElementBorderWidth(1.5f);
+      cfg.setCollapsatorBorderWidth(1.0f);
+      cfg.setConnectorWidth(2.0f);
+
       cfg.setScale(1.0d);
 
       final MindMap theModel = new MindMap(panel.getModel());
@@ -60,8 +68,35 @@ public class MMDPrint {
 
       if (renderedMap.getWidth() > paperWidthInPixels || renderedMap.getHeight() > paperHeightInPixels) {
         // split to pages
-        final int pagesHorz = Math.max(1, renderedMap.getWidth() / paperWidthInPixels + (renderedMap.getWidth()%paperWidthInPixels !=0 ? 1 : 0));
-        final int pagesVert = Math.max(1, renderedMap.getHeight() / paperHeightInPixels + (renderedMap.getHeight()% paperHeightInPixels != 0 ? 1 : 0));
+        int pagesHorz = Math.max(1, renderedMap.getWidth() / paperWidthInPixels + (renderedMap.getWidth() % paperWidthInPixels != 0 ? 1 : 0));
+        int pagesVert = Math.max(1, renderedMap.getHeight() / paperHeightInPixels + (renderedMap.getHeight() % paperHeightInPixels != 0 ? 1 : 0));
+
+        final boolean couldBeScaledForX = (renderedMap.getWidth() % paperWidthInPixels) <= Math.round(paperWidthInPixels * SMALL_SCALING_THRESHOLD);
+        final boolean couldBeScaledForY = (renderedMap.getHeight() % paperHeightInPixels) <= Math.round(paperHeightInPixels * SMALL_SCALING_THRESHOLD);
+
+        if (couldBeScaledForX || couldBeScaledForY) {
+          final double currentRatio = (double) Math.min(renderedMap.getWidth(), renderedMap.getHeight()) / (double) Math.max(renderedMap.getWidth(), renderedMap.getHeight());
+
+          final int potentialPageWidth = Math.min(renderedMap.getWidth(), Math.max(1, pagesHorz - (couldBeScaledForX ? 1 : 0)) * paperWidthInPixels);
+          final int potentialPageHeight = Math.min(renderedMap.getHeight(), Math.max(1, pagesVert - (couldBeScaledForY ? 1 : 0)) * paperHeightInPixels);
+
+          final double possibleRatio = (double) Math.min(potentialPageWidth, potentialPageHeight) / (double) Math.max(potentialPageWidth, potentialPageHeight);
+
+          if (Math.abs(currentRatio - possibleRatio) <= SCALE_RATIO_THRESHOLD) {
+            final BufferedImage scaledImage = new BufferedImage(potentialPageWidth, potentialPageHeight, BufferedImage.TYPE_INT_ARGB);
+            final Graphics2D sigfx = scaledImage.createGraphics();
+            MindMapPanel.prepareGraphicsForQuality(sigfx);
+            try {
+              sigfx.drawImage(renderedMap, 0, 0, scaledImage.getWidth(), scaledImage.getHeight(), null);
+            }
+            finally {
+              sigfx.dispose();
+            }
+            renderedMap = scaledImage;
+            pagesHorz = Math.max(1, renderedMap.getWidth() / paperWidthInPixels + (renderedMap.getWidth() % paperWidthInPixels == 0 ? 0 : 1));
+            pagesVert = Math.max(1, renderedMap.getHeight() / paperHeightInPixels + (renderedMap.getHeight() % paperHeightInPixels == 0 ? 0 : 1));
+          }
+        }
 
         final boolean centerX = pagesHorz == 1;
         final boolean centerY = pagesVert == 1;
@@ -82,16 +117,18 @@ public class MMDPrint {
                 try {
                   final int xoffset = paperWidthInPixels * pageX;
                   final int yoffset = paperHeightInPixels * pageY;
+
+                  int drawx = - xoffset;
+                  int drawy = -yoffset;
                   
                   if (centerX) {
-                    gfx.drawImage(theImage, (paperWidthInPixels-theImage.getWidth())/2, -yoffset, null);
+                    drawx = (paperWidthInPixels - theImage.getWidth()) / 2;
                   }
-                  else if (centerY) {
-                    gfx.drawImage(theImage, -xoffset, (paperHeightInPixels - theImage.getHeight()) / 2, null);
+                  
+                  if (centerY) {
+                    drawy = (paperHeightInPixels - theImage.getHeight()) / 2;
                   }
-                  else {
-                    gfx.drawImage(theImage, -xoffset, -yoffset, null);
-                  }
+                  gfx.drawImage(theImage, drawx, drawy, null);
                 }
                 finally {
                   gfx.dispose();
