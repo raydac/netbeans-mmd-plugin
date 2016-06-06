dd3aa991e253e0f08dd84aa253cd8d5a59715898
Igor Maznitsa <rrg4400@gmail.com>
2016-06-06 10:00:05 +0300

    started 1.2.1

--- mind-map/mind-map-swing-panel/src/main/java/com/igormaznitsa/mindmap/plugins/api/MindMapPlugin.java
+++ mind-map/mind-map-swing-panel/src/main/java/com/igormaznitsa/mindmap/plugins/api/MindMapPlugin.java
@@ -32,7 +32,7 @@ public interface MindMapPlugin extends Comparable<MindMapPlugin> {
   /**
    * Version of the API.
    */
-  public static final Version API = new Version("1.2.0");
+  public static final Version API = new Version("1.2.1");
   
   /**
    * Order of the plug-in among another plug-ins.
