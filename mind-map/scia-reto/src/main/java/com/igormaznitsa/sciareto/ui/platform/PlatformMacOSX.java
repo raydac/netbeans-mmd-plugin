9a8f0bf54349c6fb933f73b31ea3e24995d982b6
Igor Maznitsa <rrg4400@gmail.com>
2018-02-09 11:50:44 +0300

    #37 Can't start SciaReto under MacOS with Oracle JDK-9

--- mind-map/scia-reto/src/main/java/com/igormaznitsa/sciareto/ui/platform/PlatformMacOSX.java
+++ mind-map/scia-reto/src/main/java/com/igormaznitsa/sciareto/ui/platform/PlatformMacOSX.java
@@ -13,51 +13,48 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.igormaznitsa.sciareto.ui.platform;
 
-import java.util.Collections;
-import java.util.EnumMap;
-import java.util.Map;
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import javax.swing.UIManager;
 import com.apple.eawt.Application;
-import com.apple.eawt.ApplicationEvent;
-import com.apple.eawt.ApplicationListener;
-import com.igormaznitsa.meta.annotation.MayContainNull;
-import com.igormaznitsa.meta.common.utils.Assertions;
 import com.igormaznitsa.mindmap.model.logger.Logger;
 import com.igormaznitsa.mindmap.model.logger.LoggerFactory;
 
-class PlatformMacOSX implements Platform, ApplicationListener {
+import javax.annotation.Nonnull;
+import javax.swing.*;
+
+class PlatformMacOSX implements Platform {
 
   private final Application application;
 
   private final Logger LOGGER = LoggerFactory.getLogger(PlatformMacOSX.class);
-  
-  private final Map<PlatformMenuEvent, PlatformMenuAction> actions = Collections.synchronizedMap(new EnumMap<PlatformMenuEvent, PlatformMenuAction>(PlatformMenuEvent.class));
+  private final Object macOsxAppListener;
 
   public PlatformMacOSX() {
     this.application = Application.getApplication();
-    this.application.addApplicationListener(this);
+    Object macListener = null;
+    try {
+      final Class<?> klazz = Class.forName("com.igormaznitsa.sciareto.ui.platform.MacOSXAppListener");//NOI18N
+      macListener = klazz.getConstructor(Application.class).newInstance(this.application);
+    } catch (Exception ex) {
+      LOGGER.error("Can't register application listener, may be newest JDK with removed deprecated methods", ex);//NOI18N
+    } finally {
+      this.macOsxAppListener = macListener;
+    }
   }
 
   @Override
   public boolean registerPlatformMenuEvent(@Nonnull final PlatformMenuEvent event, @Nonnull final PlatformMenuAction action) {
-    this.actions.put(event, Assertions.assertNotNull(action));
-
-    switch (event) {
-      case ABOUT: {
-        this.application.setEnabledAboutMenu(true);
+    if (this.macOsxAppListener != null) {
+      try {
+        return (Boolean) this.macOsxAppListener.getClass().getMethod("registerPlatformMenuEvent", PlatformMenuEvent.class, PlatformMenuAction.class).invoke(this.macOsxAppListener, event, action);
+      } catch (Exception ex) {
+        LOGGER.error("Error during call com.igormaznitsa.sciareto.ui.platform.MacOSXAppListener#registerPlatformMenuEvent", ex);//NOI18N
       }
-      break;
-      case PREFERENCES: {
-        this.application.setEnabledPreferencesMenu(true);
-      }
-      break;
+    } else {
+      LOGGER.warn("Can't register platform menu event " + event + " because listener is not provided");//NOI18N
     }
-
-    return true;
+    return false;
   }
 
   @Override
@@ -75,52 +72,6 @@ class PlatformMacOSX implements Platform, ApplicationListener {
 
   @Override
   public void dispose() {
-  }
-
-  private boolean processMenuEvent(@Nonnull final PlatformMenuEvent event, @Nullable @MayContainNull final Object... args) {
-    final PlatformMenuAction action = this.actions.get(event);
-    boolean handled = false;
-    if (action == null) {
-      LOGGER.info("No registered menu event handler : " + event);       //NOI18N
-    }else {
-      handled = action.doPlatformMenuAction(event, args);
-      LOGGER.info("Processed menu event : " + event); //NOI18N
-    }
-    return handled;
-  }
-
-  @Override
-  public void handleAbout(@Nonnull final ApplicationEvent ae) {
-    ae.setHandled(processMenuEvent(PlatformMenuEvent.ABOUT));
-  }
-
-  @Override
-  public void handleOpenApplication(@Nonnull final ApplicationEvent ae) {
-  }
-
-  @Override
-  public void handleOpenFile(@Nonnull final ApplicationEvent ae) {
-    ae.setHandled(processMenuEvent(PlatformMenuEvent.OPEN_FILE, ae.getFilename()));
-  }
-
-  @Override
-  public void handlePreferences(@Nonnull final ApplicationEvent ae) {
-    ae.setHandled(processMenuEvent(PlatformMenuEvent.PREFERENCES));
-  }
-
-  @Override
-  public void handlePrintFile(@Nonnull final ApplicationEvent ae) {
-    ae.setHandled(processMenuEvent(PlatformMenuEvent.PRINT_FILE, ae.getFilename()));
-  }
-
-  @Override
-  public void handleQuit(@Nonnull final ApplicationEvent ae) {
-    ae.setHandled(processMenuEvent(PlatformMenuEvent.QUIT));
-  }
-
-  @Override
-  public void handleReOpenApplication(@Nonnull final ApplicationEvent ae) {
-    ae.setHandled(processMenuEvent(PlatformMenuEvent.REOPEN_APPLICATION));
   }
 
   @Override
