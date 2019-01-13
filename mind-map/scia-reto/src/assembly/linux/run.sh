352b755353af05590da2f8c74ff9fadcdf6e6f04
Igor Maznitsa <rrg4400@gmail.com>
2019-01-13 19:31:58 +0200

    added build of distributives with embedded jdk image

---
+++ mind-map/scia-reto/src/assembly/linux/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SCIARETO_HOME="$(dirname ${BASH_SOURCE[0]})"
+LOG_FILE=$SCIARETO_HOME/console.log
+JAVA_HOME=$SCIARETO_HOME/jre
+
+# uncomment the line below if graphics works slowly
+# JAVA_EXTRA_GFX_FLAGS="-Dsun.java2d.opengl=true"
+
+JAVA_FLAGS="-client -XX:+IgnoreUnrecognizedVMOptions --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED -Dsun.java2d.dpiaware=true -Dswing.aatext=true -Dawt.useSystemAAFontSettings=on"
+
+JAVA_RUN=$JAVA_HOME/bin/java
+
+echo \$JAVA_RUN=$JAVA_RUN &>$LOG_FILE
+
+echo ------JAVA_VERSION------ &>>$LOG_FILE
+
+$JAVA_RUN -version &>>$LOG_FILE
+
+echo ------------------------ &>>$LOG_FILE
+
+$JAVA_RUN $JAVA_FLAGS $JAVA_EXTRA_GFX_FLAGS -jar $SCIARETO_HOME/scia-reto.jar $@ &>>$LOG_FILE&
