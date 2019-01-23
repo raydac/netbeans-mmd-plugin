e001386e531a255df3836752f7e4379b07059284
Igor Maznitsa <rrg4400@gmail.com>
2019-01-23 23:01:59 +0200

    improved emoticon loading

--- mind-map/mind-map-swing-panel/src/main/java/com/igormaznitsa/mindmap/swing/panel/utils/MiscIcons.java
+++ mind-map/mind-map-swing-panel/src/main/java/com/igormaznitsa/mindmap/swing/panel/utils/MiscIcons.java
@@ -19,7 +19,6 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import javax.annotation.Nonnull;
@@ -29,59 +28,93 @@ import org.apache.commons.io.IOUtils;
 import com.igormaznitsa.meta.annotation.MustNotContainNull;
 import com.igormaznitsa.mindmap.model.logger.Logger;
 import com.igormaznitsa.mindmap.model.logger.LoggerFactory;
+import java.util.Collections;
+import java.util.HashMap;
 
 public class MiscIcons {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(MiscIcons.class);
 
+  private static final Map<String, ImageContainer> IMAGE_CACHE;
   private static final String[] ICON_NAMES;
 
   static {
     final InputStream iconListReadStream = MiscIcons.class.getResourceAsStream("/com/igormaznitsa/mindmap/swing/miscicons/icon.lst");
+
+    final Map<String, ImageContainer> imageContainers = new HashMap<String, ImageContainer>();
+
     try {
+      imageContainers.put("empty", new ImageContainer("empty"));
+
       final List<String> lines = IOUtils.readLines(iconListReadStream, "UTF-8");
       ICON_NAMES = lines.toArray(new String[lines.size()]);
+      for (final String icon : ICON_NAMES) {
+        imageContainers.put(icon, new ImageContainer(icon));
+      }
     } catch (Exception ex) {
       throw new Error("Can't read list of icons", ex);
     } finally {
       IOUtils.closeQuietly(iconListReadStream);
     }
+
+    IMAGE_CACHE = Collections.unmodifiableMap(imageContainers);
+
+    final Thread loadingDaemon = new Thread(new Runnable() {
+      @Override
+      public void run() {
+        LOGGER.info("Loadin daemon started");
+        for (final String s : ICON_NAMES) {
+          findForName(s);
+        }
+        LOGGER.info("Loadin daemon completed");
+      }
+    }, "mindmap-emoticon-loading");
+    loadingDaemon.setDaemon(true);
+    loadingDaemon.setPriority(Thread.MIN_PRIORITY);
+    loadingDaemon.start();
   }
 
-  private static final Map<String, Image> IMAGE_CACHE = new HashMap<String, Image>();
+  private static final class ImageContainer {
 
-  @Nullable
-  private static Image loadImage(@Nonnull final String name) {
-    if ("empty".equals(name)) {
-      final BufferedImage result = new BufferedImage(32, 32, BufferedImage.TYPE_INT_ARGB);
-      return result;
+    private final String name;
+    private volatile Image image;
+
+    private ImageContainer(@Nonnull final String name) {
+      this.name = name;
+      if ("empty".equals(name)) {
+        this.image = new BufferedImage(32, 32, BufferedImage.TYPE_INT_ARGB);
+      }
     }
-    final InputStream in = MiscIcons.class.getResourceAsStream("/com/igormaznitsa/mindmap/swing/miscicons/" + name + ".png");
-    if (in == null) {
-      return null;
+
+    @Nullable
+    private synchronized Image getImage() {
+      if (this.image == null) {
+        this.image = loadImage(this.name);
+      }
+      return this.image;
     }
-    try {
-      return ImageIO.read(in);
-    } catch (IOException ex) {
-      LOGGER.error("IO exception for icon '" + name + '\'');
-      return null;
-    } finally {
-      IOUtils.closeQuietly(in);
+
+    @Nullable
+    private Image loadImage(@Nonnull final String name) {
+      final InputStream in = MiscIcons.class.getResourceAsStream("/com/igormaznitsa/mindmap/swing/miscicons/" + name + ".png");
+      if (in == null) {
+        return null;
+      }
+      try {
+        return ImageIO.read(in);
+      } catch (IOException ex) {
+        LOGGER.error("IO exception for icon '" + name + '\'');
+        return null;
+      } finally {
+        IOUtils.closeQuietly(in);
+      }
     }
   }
 
   @Nullable
   public static Image findForName(@Nonnull final String name) {
-    synchronized (IMAGE_CACHE) {
-      Image img = IMAGE_CACHE.get(name);
-      if (img == null) {
-        img = loadImage(name);
-        if (img != null) {
-          IMAGE_CACHE.put(name, img);
-        }
-      }
-      return img;
-    }
+    final ImageContainer result = IMAGE_CACHE.get(name);
+    return result == null ? null : result.getImage();
   }
 
   @Nonnull
