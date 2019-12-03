9e727748f16afb5f675c2de4fa6807d2da32ef73
Igor Maznitsa <rrg4400@gmail.com>
2019-12-04 01:13:58 +0200

    added parser for log of KS

---
+++ mind-map/scia-reto/src/main/java/com/igormaznitsa/sciareto/ui/editors/KsTplGraph.java
@@ -0,0 +1,306 @@
+package com.igormaznitsa.sciareto.ui.editors;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Optional;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public class KsTplGraph {
+
+  private static final Pattern MAIN_PATTERN = Pattern.compile("^(?:(<-+|-+>)|(?:([\\w\\-\\s]+?):))(.*)$", Pattern.CASE_INSENSITIVE);
+  private static final Pattern ID_TAIL_PATTERN = Pattern.compile("^\\s*([\\S]+)(?:\\s+(.+?))?\\s*$");
+  private static final Pattern DATA_COMMENT_PATTERN = Pattern.compile("^\\s*(?:\\(([^)]*)\\))?(.*)\\s*$");
+  private static final Pattern DATA_FINDER = Pattern.compile(",?\\s*([a-z_\\-]+?)\\s*:\\s*\\[(.*?)]");
+
+  private static final String NONE = "none";
+  private final List<Topologies> topologies = new ArrayList<>();
+
+  public KsTplGraph(@Nonnull final String script) {
+    final List<ParsedItem> foundItems = new ArrayList<>();
+    for (final String s : script.split("\\n")) {
+      String lineText = s.trim();
+      if ("topology".equalsIgnoreCase(lineText)) {
+        lineText = "Topologies:";
+      }
+      if ("sub-topologies:".equalsIgnoreCase(lineText)) {
+        lineText = "";
+      }
+
+      if (lineText.isEmpty()) {
+        continue;
+      }
+      final Matcher matcher = MAIN_PATTERN.matcher(lineText);
+      if (matcher.find()) {
+        final String srcDst = matcher.group(1);
+        final String itemName = matcher.group(2);
+        final String tail = matcher.group(3);
+
+        if (srcDst != null) {
+          if (foundItems.isEmpty()) {
+            throw new IllegalArgumentException(String.format("Found '%s' without element", srcDst));
+          } else {
+            final List<String> identifiers = Arrays.stream(tail.split(","))
+                .map(String::trim)
+                .filter(x -> !x.isEmpty())
+                .collect(Collectors.toList());
+
+            if (srcDst.endsWith(">")) {
+              foundItems.get(foundItems.size() - 1).to.addAll(identifiers);
+            } else {
+              foundItems.get(foundItems.size() - 1).from.addAll(identifiers);
+            }
+          }
+        } else {
+          foundItems.add(new ParsedItem(lineText, itemName, tail));
+        }
+      } else {
+        throw new IllegalArgumentException("Can't parse line: " + lineText);
+      }
+    }
+    final Map<String, TopologyElement> topologyElementMaps = new HashMap<>();
+    for (final ParsedItem i : foundItems) {
+      final String lcName = i.name.toLowerCase(Locale.ENGLISH);
+      final TopologyElement newElement;
+      if ("topologies".equals(lcName)) {
+        this.topologies.add(new Topologies(i.tail));
+        newElement = null;
+      } else if (lcName.startsWith("sub") && lcName.endsWith("topology")) {
+        if (this.topologies.isEmpty()) {
+          this.topologies.add(new Topologies(null));
+        }
+        final Topologies curTop = this.topologies.get(this.topologies.size() - 1);
+        curTop.subTopologies.add(new SubTopology(i));
+        newElement = null;
+      } else {
+        if (this.topologies.isEmpty()) {
+          this.topologies.add(new Topologies(null));
+        }
+        newElement = new TopologyElement(i);
+        final Topologies curTop = this.topologies.get(this.topologies.size() - 1);
+        if (curTop.subTopologies.isEmpty()) {
+          curTop.orphans.add(newElement);
+        } else {
+          curTop.subTopologies.get(curTop.subTopologies.size() - 1).children.put(newElement.id, newElement);
+        }
+      }
+      if (newElement != null) {
+        topologyElementMaps.put(newElement.id, newElement);
+      }
+    }
+
+    this.topologies.forEach(x -> x.link(topologyElementMaps));
+  }
+
+  public int size() {
+    return this.topologies.stream().mapToInt(x -> x.size()).sum();
+  }
+
+  @Override
+  public String toString() {
+    final StringBuilder builder = new StringBuilder();
+    this.topologies.forEach(x -> builder.append(x).append("\n"));
+    return builder.toString();
+  }
+
+  @Nonnull
+  public Optional<TopologyElement> findForId(@Nonnull final String id) {
+    TopologyElement result = null;
+    for (final Topologies t : this.topologies) {
+      result = t.findForId(id);
+      if (result != null) {
+        break;
+      }
+    }
+    return Optional.ofNullable(result);
+  }
+
+  private static final class ParsedItem {
+
+    final String name;
+    final String tail;
+    final List<String> from;
+    final List<String> to;
+    final String orig;
+
+    public ParsedItem(@Nonnull final String orig, @Nonnull final String name, @Nonnull final String tail) {
+      this.orig = orig;
+      this.name = name;
+      this.tail = tail;
+      this.from = new ArrayList<>();
+      this.to = new ArrayList<>();
+    }
+  }
+
+  public static class TopologyElement {
+
+    final String type;
+    final String id;
+    final String comment;
+    final Map<String, List<String>> dataItems = new HashMap<>();
+    final List<TopologyElement> to = new ArrayList<>();
+    final List<TopologyElement> from = new ArrayList<>();
+    final ParsedItem parsedItem;
+
+    public TopologyElement(@Nonnull final ParsedItem parsedItem) {
+      this.parsedItem = parsedItem;
+      this.type = parsedItem.name;
+      final Matcher idtail = ID_TAIL_PATTERN.matcher(parsedItem.tail);
+      if (idtail.find()) {
+        this.id = idtail.group(1);
+        final String tail = idtail.group(2);
+        if (tail == null) {
+          this.comment = "";
+        } else {
+          final Matcher dataComment = DATA_COMMENT_PATTERN.matcher(tail);
+          if (dataComment.find()) {
+            final String data = dataComment.group(1);
+            final String foundComment = dataComment.group(2);
+            if (data != null) {
+              final Matcher dataFinder = DATA_FINDER.matcher(data);
+              while (dataFinder.find()) {
+                final String foundType = dataFinder.group(1);
+                final String foundDataList = dataFinder.group(2);
+                final List<String> foundItems = Arrays.stream(foundDataList.split(",")).map(x -> x.trim()).filter(x -> !x.isEmpty()).collect(Collectors.toList());
+                this.dataItems.put(foundType, foundItems);
+              }
+            }
+            this.comment = foundComment == null ? "" : foundComment;
+          } else {
+            this.comment = tail;
+          }
+        }
+      } else {
+        throw new IllegalArgumentException("Can't extract ID from line: " + parsedItem.orig);
+      }
+    }
+
+    void link(@Nonnull final Map<String, TopologyElement> map) {
+      this.from.clear();
+      this.to.clear();
+
+      this.parsedItem.to.forEach(x -> {
+        if (!NONE.equalsIgnoreCase(x)) {
+          final TopologyElement element = map.get(x);
+          if (element == null) {
+            throw new IllegalArgumentException("Can't find any element named " + x);
+          }
+          this.to.add(element);
+        }
+      });
+
+      this.parsedItem.from.forEach(x -> {
+        if (!NONE.equalsIgnoreCase(x)) {
+          final TopologyElement element = map.get(x);
+          if (element == null) {
+            throw new IllegalArgumentException("Can't find any element named " + x);
+          }
+          this.from.add(element);
+        }
+      });
+    }
+
+    @Override
+    public String toString() {
+      final StringBuilder builder = new StringBuilder();
+      builder.append(String.format("%s: %s '%s data: %s", this.id, this.type, this.comment, this.dataItems));
+      return builder.toString();
+    }
+  }
+
+  private static final class SubTopology extends TopologyElement {
+
+    final Map<String, TopologyElement> children = new TreeMap<>();
+
+    SubTopology(@Nonnull final ParsedItem parsedItem) {
+      super(parsedItem);
+    }
+
+    @Override
+    void link(@Nonnull final Map<String, TopologyElement> map) {
+      this.children.forEach((k, v) -> v.link(map));
+    }
+
+    @Override
+    public String toString() {
+      final StringBuilder builder = new StringBuilder();
+      builder.append(super.toString());
+      this.children.forEach((k, v) -> {
+        builder.append('\n').append(v);
+      });
+      return builder.toString();
+    }
+
+    public int size() {
+      return this.children.size();
+    }
+
+    @Nullable
+    public TopologyElement findForId(@Nonnull final String id) {
+      return this.children.get(id);
+    }
+  }
+
+  private static final class Topologies {
+
+    final String comment;
+    final List<SubTopology> subTopologies = new ArrayList<>();
+    final List<TopologyElement> orphans = new ArrayList<>();
+
+    public Topologies(@Nullable final String comment) {
+      this.comment = comment == null ? "" : comment;
+    }
+
+    void link(@Nonnull final Map<String, TopologyElement> map) {
+      this.subTopologies.forEach(v -> v.link(map));
+    }
+
+    @Override
+    public String toString() {
+      final StringBuilder builder = new StringBuilder();
+      builder.append("Topologies: ").append(this.comment);
+      this.subTopologies.forEach(x -> {
+        builder.append('\n').append(x.toString());
+      });
+      builder.append("\nOrphans:");
+      this.orphans.forEach(x -> {
+        builder.append('\n').append(x.toString());
+      });
+      return builder.toString();
+    }
+
+    public int size() {
+      return this.subTopologies.stream().mapToInt(SubTopology::size).sum() + this.orphans.size();
+    }
+
+    @Nullable
+    public TopologyElement findForId(@Nonnull String id) {
+      TopologyElement result = null;
+      for (final SubTopology t : this.subTopologies) {
+        result = t.findForId(id);
+        if (result != null) {
+          break;
+        }
+      }
+
+      if (result == null) {
+        for (final TopologyElement e : this.orphans) {
+          if (id.equals(e.id)) {
+            result = e;
+            break;
+          }
+        }
+      }
+
+      return result;
+    }
+  }
+}
---
+++ mind-map/scia-reto/src/test/java/com/igormaznitsa/sciareto/ui/editors/KsTplGraphTest.java
@@ -0,0 +1,254 @@
+/*
+ * Copyright (C) 2019 Igor Maznitsa.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+
+package com.igormaznitsa.sciareto.ui.editors;
+
+import static java.time.Duration.ofMinutes;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+import java.time.Instant;
+import java.util.Optional;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.JoinWindows;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.ValueTransformerWithKey;
+import org.apache.kafka.streams.processor.AbstractProcessor;
+import org.apache.kafka.streams.processor.ProcessorContext;
+import org.apache.kafka.streams.processor.WallclockTimestampExtractor;
+import org.apache.kafka.streams.state.KeyValueStore;
+import org.apache.kafka.streams.state.StoreBuilder;
+import org.apache.kafka.streams.state.Stores;
+import org.junit.Test;
+
+public class KsTplGraphTest {
+
+  @Test
+  public void testFromTopology() {
+    final Topology topology = new Topology();
+    topology.addSource("SomeSource", "topic1", "topic2");
+    topology.addProcessor("Processor1", () -> new FakeProcessor(), "SomeSource");
+    topology.addProcessor("Processor2", () -> new FakeProcessor(), "Processor1");
+    topology.addProcessor("Processor3", () -> new FakeProcessor(), "Processor2");
+    topology.addSink("TheSink", "FinalTopic", "Processor3");
+
+    final String src = topology.describe().toString();
+    System.out.println(src);
+
+    final KsTplGraph graph = new KsTplGraph(src);
+    System.out.println(graph);
+
+    assertEquals(5, graph.size());
+  }
+
+  @Test
+  public void testSimple() {
+    final String text = "Topologies:\n"
+        + "   Sub-topology: 0\n"
+        + "    Source: KSTREAM-SOURCE-0000000000 (topics: [a])\n"
+        + "      --> KSTREAM-MERGE-0000000001\n"
+        + "\n"
+        + "    Processor: KSTREAM-MERGE-0000000001 (stores: [])\n"
+        + "      --> KSTREAM-SINK-0000000002\n"
+        + "      <-- KSTREAM-SOURCE-0000000000\n"
+        + "\n"
+        + "    Sink: KSTREAM-SINK-0000000002 (topic: c)\n"
+        + "      <-- KSTREAM-MERGE-0000000001";
+
+    assertEquals(3, new KsTplGraph(text).size());
+  }
+
+  @Test
+  public void testTwoSources() {
+    final String text = "Topologies:\n"
+        + "   Sub-topology: 0\n"
+        + "    Source: KSTREAM-SOURCE-0000000000 (topics: [a])\n"
+        + "      --> KSTREAM-MERGE-0000000002\n"
+        + "\n"
+        + "    Source: KSTREAM-SOURCE-0000000001 (topics: [b])\n"
+        + "      --> KSTREAM-MERGE-0000000002\n"
+        + "\n"
+        + "    Processor: KSTREAM-MERGE-0000000002 (stores: [])\n"
+        + "      --> KSTREAM-SINK-0000000003\n"
+        + "      <-- KSTREAM-SOURCE-0000000000, KSTREAM-SOURCE-0000000001\n"
+        + "\n"
+        + "    Sink: KSTREAM-SINK-0000000003 (topic: c)\n"
+        + "      <-- KSTREAM-MERGE-0000000002";
+
+    assertEquals(4, new KsTplGraph(text).size());
+  }
+
+  @Test
+  public void testManualText() {
+    final String text = "Topologies:\n"
+        + "   Sub-topology: 0\n"
+        + "    Source: sensor-a (topics: [topic-a])\n"
+        + "      --> to-the-world\n"
+        + "    Source: sensor-b (topics: [topic-b])\n"
+        + "      --> to-the-world\n"
+        + "    Sink: to-the-world (topic: output-topic)\n"
+        + "      <-- sensor-a, sensor-b";
+
+    final KsTplGraph graph = new KsTplGraph(text);
+
+    assertEquals(3, graph.size());
+    Optional<KsTplGraph.TopologyElement> source = graph.findForId("sensor-a");
+    assertTrue(source.isPresent());
+    assertEquals(1, source.get().dataItems.get("topics").size());
+    assertTrue(source.get().from.isEmpty());
+    assertEquals(1, source.get().to.size());
+    assertEquals("to-the-world", source.get().to.get(0).id);
+
+    Optional<KsTplGraph.TopologyElement> sink = graph.findForId("to-the-world");
+    assertTrue(sink.isPresent());
+    assertEquals(2, sink.get().from.size());
+  }
+
+  @Test
+  public void testKsDsl1() {
+    final StreamsBuilder builder = new StreamsBuilder();
+    final KStream<String, String> streamOne = builder.stream("input-topic-one");
+    final KStream<String, String> streamTwo = builder.stream("input-topic-two");
+    final KStream<String, String> streamOneNewKey = streamOne.selectKey((k, v) -> v.substring(0, 5));
+    final KStream<String, String> streamTwoNewKey = streamTwo.selectKey((k, v) -> v.substring(4, 9));
+    streamOneNewKey.join(streamTwoNewKey, (v1, v2) -> v1 + ":" + v2, JoinWindows.of(ofMinutes(5).toMillis())).to("joined-output");
+
+    final Topology topology = builder.build();
+    final String text = topology.describe().toString();
+    System.out.println(text);
+
+    final KsTplGraph parsed = new KsTplGraph(text);
+    assertEquals(16, parsed.size());
+    System.out.println(parsed.toString());
+  }
+
+  @Test
+  public void testKsDsl2() {
+
+    final String storeName = "stateStore";
+    final String globalStoreName = "glob-stateStore";
+    final StreamsBuilder builder = new StreamsBuilder();
+    final StoreBuilder<KeyValueStore<String, String>> storeBuilder = Stores.keyValueStoreBuilder(
+        Stores.persistentKeyValueStore(storeName),
+        Serdes.String(),
+        Serdes.String());
+    final StoreBuilder<KeyValueStore<String, String>> globalStoreBuilder = Stores.keyValueStoreBuilder(
+        Stores.persistentKeyValueStore(globalStoreName),
+        Serdes.String(),
+        Serdes.String());
+    builder.addGlobalStore(globalStoreBuilder, "some-global-topic", Consumed.with(Serdes.Short(), Serdes.String(), new WallclockTimestampExtractor(), Topology.AutoOffsetReset.EARLIEST), () -> new FakeProcessor());
+    builder.addStateStore(storeBuilder);
+    builder.<String, String>stream("input")
+        .filter((k, v) -> v.endsWith("FOO"))
+        .transformValues(() -> new SimpleValueTransformer(storeName), storeName)
+        .to("output");
+
+    final Topology topology = builder.build();
+    final String text = topology.describe().toString();
+    System.out.println(text);
+
+    final KsTplGraph parsed = new KsTplGraph(text);
+    assertEquals(6, parsed.size());
+  }
+
+  @Test
+  public void testFromGitTopologyVisualizer() {
+    final String text = "Topology\n"
+        + "Sub-topologies:\n"
+        + "Sub-topology: 0\n"
+        + "\tSource:  KSTREAM-SOURCE-0000000000 (topics: [conversation-meta])\n"
+        + "\t--> KSTREAM-TRANSFORM-0000000001\n"
+        + "\tProcessor: KSTREAM-TRANSFORM-0000000001 (stores: [conversation-meta-state])\n"
+        + "\t--> KSTREAM-KEY-SELECT-0000000002\n"
+        + "\t<-- KSTREAM-SOURCE-0000000000\n"
+        + "\tProcessor: KSTREAM-KEY-SELECT-0000000002 (stores: [])\n"
+        + "\t--> KSTREAM-FILTER-0000000005\n"
+        + "\t<-- KSTREAM-TRANSFORM-0000000001\n"
+        + "\tProcessor: KSTREAM-FILTER-0000000005 (stores: [])\n"
+        + "\t--> KSTREAM-SINK-0000000004\n"
+        + "\t<-- KSTREAM-KEY-SELECT-0000000002\n"
+        + "\tSink: KSTREAM-SINK-0000000004 (topic: count-resolved-repartition)\n"
+        + "\t<-- KSTREAM-FILTER-0000000005\n"
+        + "Sub-topology: 1\n"
+        + "\tSource: KSTREAM-SOURCE-0000000006 (topics: [count-resolved-repartition])\n"
+        + "\t--> KSTREAM-AGGREGATE-0000000003\n"
+        + "\tProcessor: KSTREAM-AGGREGATE-0000000003 (stores: [count-resolved])\n"
+        + "\t--> KTABLE-TOSTREAM-0000000007\n"
+        + "\t<-- KSTREAM-SOURCE-0000000006\n"
+        + "\tProcessor: KTABLE-TOSTREAM-0000000007 (stores: [])\n"
+        + "\t--> KSTREAM-SINK-0000000008\n"
+        + "\t<-- KSTREAM-AGGREGATE-0000000003\n"
+        + "\tSink: KSTREAM-SINK-0000000008 (topic: streams-count-resolved)\n"
+        + "\t<-- KTABLE-TOSTREAM-0000000007\n"
+        + "\t\t\t";
+
+    final KsTplGraph graph = new KsTplGraph(text);
+    System.out.println(graph);
+    assertEquals(9, graph.size());
+  }
+
+  static class SimpleValueTransformer implements ValueTransformerWithKey<String, String, String> {
+
+    private String storeName;
+    private KeyValueStore<String, String> store;
+
+    public SimpleValueTransformer(String storeName) {
+      this.storeName = storeName;
+    }
+
+    @Override
+    public void init(final ProcessorContext context) {
+      store = (KeyValueStore) context.getStateStore(storeName);
+    }
+
+    @Override
+    public String transform(final String key, final String value) {
+      String persistedValue = store.get(key);
+      final String updatedValue = value + "_" + Instant.now().toString();
+
+      if (persistedValue == null) {
+        persistedValue = updatedValue;
+      }
+
+      store.put(key, updatedValue);
+      return persistedValue;
+    }
+
+    @Override
+    public void close() {
+
+    }
+  }
+
+  private class FakeProcessor extends AbstractProcessor<Object, Object> {
+    public FakeProcessor() {
+      super();
+    }
+
+    @Override
+    public void process(Object k, Object v) {
+      throw new UnsupportedOperationException("Not supported yet.");
+    }
+  }
+
+}
