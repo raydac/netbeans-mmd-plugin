f4b720fc57f9aa8a8f990e7e8e52618a8668bf2f
Igor Maznitsa <rrg4400@gmail.com>
2020-05-24 14:46:48 +0300

    updated desktop generating script

--- mind-map/scia-reto/src/assembly/linux/make_desktop.sh
+++ mind-map/scia-reto/src/assembly/linux/make_desktop.sh
@@ -6,18 +6,18 @@ APP_HOME="$(realpath $(dirname ${BASH_SOURCE[0]}))"
 TARGET=$APP_HOME/scia-reto-editor.desktop
 
 echo [Desktop Entry] > $TARGET
+echo Version=1.0 >> $TARGET
 echo Encoding=UTF-8 >> $TARGET
-echo Version=1.4.9 >> $TARGET
 echo Type=Application >> $TARGET
 echo Name=SciaReto >> $TARGET
 echo GenericName=SciaReto mind-map editor >> $TARGET
-echo Icon="$APP_HOME/icon.svg" >> $TARGET
-echo Exec="$APP_HOME/run.sh" %f >> $TARGET
+echo Icon=\"$APP_HOME/icon.svg\" >> $TARGET
+echo Exec=\"$APP_HOME/run.sh\" %f >> $TARGET
 echo Comment=Free mind-map editor with PlantUML support >> $TARGET
-echo Categories=Development;Editor; >> $TARGET
+echo "Categories=Office;FlowChart;" >> $TARGET
 echo Terminal=false >> $TARGET
-echo StartupWMClass=scia-reto-mind-map-editor >> $TARGET
-echo OnlyShowIn=Old; >> $TARGET
+echo StartupWMClass=scia-reto-editor >> $TARGET
+echo "OnlyShowIn=Old;" >> $TARGET
 echo StartupNotify=true >> $TARGET
 
 echo Desktop script has been generated: $TARGET
@@ -27,13 +27,14 @@ if [ -d ~/.gnome/apps ]; then
     cp -f $TARGET ~/.gnome/apps
 fi
 
-if [ -d ~/.local/share/applications ]; then
-    echo copy to ~/.local/share/applications
-    cp -f $TARGET ~/.local/share/applications
-fi
-
 if [ -d ~/Desktop ]; then
     echo copy to ~/Desktop
     cp -f $TARGET ~/Desktop
 fi
 
+if [ -d ~/.local/share/applications ]; then
+    echo copy to ~/.local/share/applications
+    cp -f $TARGET ~/.local/share/applications
+
+    update-desktop-database ~/.local/share/applications
+fi
