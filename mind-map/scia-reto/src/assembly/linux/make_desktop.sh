3204bbda4b68cef2cff138f6d5e619a091182072
Igor Maznitsa <rrg4400@gmail.com>
2020-05-24 14:03:19 +0300

    updated desktop icon for mac and linux

--- mind-map/scia-reto/src/assembly/linux/make_desktop.sh
+++ mind-map/scia-reto/src/assembly/linux/make_desktop.sh
@@ -2,23 +2,38 @@
 
 # Script just generates free desktop descriptor to start application
 
-SCIARETO_HOME="$(realpath $(dirname ${BASH_SOURCE[0]}))"
-TARGET=$SCIARETO_HOME/sciareto.desktop
+APP_HOME="$(realpath $(dirname ${BASH_SOURCE[0]}))"
+TARGET=$APP_HOME/scia-reto-editor.desktop
 
 echo [Desktop Entry] > $TARGET
 echo Encoding=UTF-8 >> $TARGET
-echo Name=Scia Reto >> $TARGET
-echo Comment=Free mind map editor >> $TARGET
-echo GenericName=Free mind map editor >> $TARGET
-echo Exec=$SCIARETO_HOME/run.sh >> $TARGET
-echo Terminal=false >> $TARGET
+echo Version=1.4.9 >> $TARGET
 echo Type=Application >> $TARGET
-echo Icon=$SCIARETO_HOME/icon.png >> $TARGET
-echo Categories=Application; >> $TARGET
-echo StartupWMClass=Scia Reto editor >> $TARGET
+echo Name=SciaReto >> $TARGET
+echo GenericName=SciaReto mind-map editor >> $TARGET
+echo Icon="$APP_HOME/icon.svg" >> $TARGET
+echo Exec="$APP_HOME/run.sh" %f >> $TARGET
+echo Comment=Free mind-map editor with PlantUML support >> $TARGET
+echo Categories=Development;Editor; >> $TARGET
+echo Terminal=false >> $TARGET
+echo StartupWMClass=scia-reto-mind-map-editor >> $TARGET
+echo OnlyShowIn=Old; >> $TARGET
 echo StartupNotify=true >> $TARGET
 
-
-chmod +x $TARGET
-
 echo Desktop script has been generated: $TARGET
+
+if [ -d ~/.gnome/apps ]; then
+    echo copy to ~/.gnome/apps
+    cp -f $TARGET ~/.gnome/apps
+fi
+
+if [ -d ~/.local/share/applications ]; then
+    echo copy to ~/.local/share/applications
+    cp -f $TARGET ~/.local/share/applications
+fi
+
+if [ -d ~/Desktop ]; then
+    echo copy to ~/Desktop
+    cp -f $TARGET ~/Desktop
+fi
+
