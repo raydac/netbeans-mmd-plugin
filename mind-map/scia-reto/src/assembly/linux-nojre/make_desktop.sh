32414272b4f3a2c8d7ee9b8ea3719223c50bc2f4
Igor Maznitsa <rrg4400@gmail.com>
2020-05-24 18:51:48 +0300

    updated desktop generating script

--- mind-map/scia-reto/src/assembly/linux-nojre/make_desktop.sh
+++ mind-map/scia-reto/src/assembly/linux-nojre/make_desktop.sh
@@ -6,19 +6,18 @@ APP_HOME="$(realpath $(dirname ${BASH_SOURCE[0]}))"
 TARGET=$APP_HOME/scia-reto-editor.desktop
 
 echo [Desktop Entry] > $TARGET
-echo Version=1.0 >> $TARGET
+echo Version=1.1 >> $TARGET
 echo Encoding=UTF-8 >> $TARGET
 echo Type=Application >> $TARGET
-echo Name=SciaReto >> $TARGET
-echo GenericName=SciaReto mind-map editor >> $TARGET
-echo Icon=\"$APP_HOME/icon.svg\" >> $TARGET
+echo Name=Scia-Reto editor >> $TARGET
+echo GenericName=Scia-Reto mind-map editor >> $TARGET
+echo Comment=Free open source mind-map editor with PlantUML support >> $TARGET
+echo Icon=$APP_HOME/icon.svg >> $TARGET
 echo Exec=\"$APP_HOME/run.sh\" %f >> $TARGET
-echo Comment=Free mind-map editor with PlantUML support >> $TARGET
 echo "Categories=Office;FlowChart;" >> $TARGET
+echo "Keywords=mind;map;scia;reto;uml;plant;kstream;kafka;" >> $TARGET
 echo Terminal=false >> $TARGET
-echo StartupWMClass=scia-reto-editor >> $TARGET
-echo "OnlyShowIn=Old;" >> $TARGET
-echo StartupNotify=true >> $TARGET
+echo StartupWMClass=scia.reto.editor >> $TARGET
 
 echo Desktop script has been generated: $TARGET
 
@@ -35,6 +34,4 @@ fi
 if [ -d ~/.local/share/applications ]; then
     echo copy to ~/.local/share/applications
     cp -f $TARGET ~/.local/share/applications
-
-    update-desktop-database ~/.local/share/applications
 fi
