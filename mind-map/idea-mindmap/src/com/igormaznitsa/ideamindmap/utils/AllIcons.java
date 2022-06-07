e0dab2649cbe7910a6b6148d2d9d5bfbcef1bc43
Igor Maznitsa <rrg4400@gmail.com>
2022-06-07 10:18:19 +0300

    replaced deprecated method call

--- mind-map/idea-mindmap/src/com/igormaznitsa/ideamindmap/utils/AllIcons.java
+++ mind-map/idea-mindmap/src/com/igormaznitsa/ideamindmap/utils/AllIcons.java
@@ -16,91 +16,93 @@
 
 package com.igormaznitsa.ideamindmap.utils;
 
+import static com.intellij.openapi.util.IconLoader.findIcon;
+
 import com.intellij.openapi.util.IconLoader;
 import javax.swing.Icon;
 
 public class AllIcons {
   public static class Logo {
-    public static final Icon MINDMAP = IconLoader.getIcon("/icons/logo/logo.png");
+    public static final Icon MINDMAP = findIcon("/icons/logo/logo.png");
   }
 
   public static class FindText {
-    public static final Icon CASE = IconLoader.getIcon("/icons/find/case16.png");
-    public static final Icon FILE = IconLoader.getIcon("/icons/find/disk16.png");
-    public static final Icon NOTE = IconLoader.getIcon("/icons/find/note16.png");
-    public static final Icon TEXT = IconLoader.getIcon("/icons/find/text16.png");
-    public static final Icon URL = IconLoader.getIcon("/icons/find/url16.png");
-    public static final Icon NEXT = IconLoader.getIcon("/icons/find/resultset_next.png");
-    public static final Icon PREV = IconLoader.getIcon("/icons/find/resultset_previous.png");
-    public static final Icon CLOSE = IconLoader.getIcon("/icons/find/nimbusCloseFrame.png");
+    public static final Icon CASE = findIcon("/icons/find/case16.png");
+    public static final Icon FILE = findIcon("/icons/find/disk16.png");
+    public static final Icon NOTE = findIcon("/icons/find/note16.png");
+    public static final Icon TEXT = findIcon("/icons/find/text16.png");
+    public static final Icon URL = findIcon("/icons/find/url16.png");
+    public static final Icon NEXT = findIcon("/icons/find/resultset_next.png");
+    public static final Icon PREV = findIcon("/icons/find/resultset_previous.png");
+    public static final Icon CLOSE = findIcon("/icons/find/nimbusCloseFrame.png");
   }
 
   public static class ImportExport {
-    public static final Icon TXT2MM = IconLoader.getIcon("/icons/txt2mm.png");
-    public static final Icon XMIND2MM = IconLoader.getIcon("/icons/xmind2mm.png");
-    public static final Icon ORGMODE = IconLoader.getIcon("/icons/orgmode.png");
-    public static final Icon COGGLE = IconLoader.getIcon("/icons/coggle.png");
-    public static final Icon NOVAMIND = IconLoader.getIcon("/icons/novamind.png");
+    public static final Icon TXT2MM = findIcon("/icons/txt2mm.png");
+    public static final Icon XMIND2MM = findIcon("/icons/xmind2mm.png");
+    public static final Icon ORGMODE = findIcon("/icons/orgmode.png");
+    public static final Icon COGGLE = findIcon("/icons/coggle.png");
+    public static final Icon NOVAMIND = findIcon("/icons/novamind.png");
   }
 
   public static class File {
-    public static final Icon MINDMAP = IconLoader.getIcon("/icons/mmdfile.png");
-    public static final Icon FOLDER = IconLoader.getIcon("/icons/folder.png");
+    public static final Icon MINDMAP = findIcon("/icons/mmdfile.png");
+    public static final Icon FOLDER = findIcon("/icons/folder.png");
   }
 
   public static class Tree {
-    public static final Icon DOCUMENT = IconLoader.getIcon("/icons/document.png");
-    public static final Icon BLUEBALL = IconLoader.getIcon("/icons/blueball.png");
-    public static final Icon GOLDBALL = IconLoader.getIcon("/icons/goldball.png");
+    public static final Icon DOCUMENT = findIcon("/icons/document.png");
+    public static final Icon BLUEBALL = findIcon("/icons/blueball.png");
+    public static final Icon GOLDBALL = findIcon("/icons/goldball.png");
   }
 
   public static class Buttons {
-    public static final Icon CROSS = IconLoader.getIcon("/icons/cross.png");
-    public static final Icon CANCEL = IconLoader.getIcon("/icons/cancel.png");
-    public static final Icon TICK = IconLoader.getIcon("/icons/tick.png");
-    public static final Icon QUESTION = IconLoader.getIcon("/icons/question.png");
-    public static final Icon EXPANDALL = IconLoader.getIcon("/icons/toggle_expand.png");
-    public static final Icon COLLAPSEALL = IconLoader.getIcon("/icons/toggle.png");
-    public static final Icon SELECT = IconLoader.getIcon("/icons/select.png");
-    public static final Icon FILE_LINK_BIG = IconLoader.getIcon("/icons/file_link.png");
-    public static final Icon URL_LINK_BIG = IconLoader.getIcon("/icons/url_link.png");
-    public static final Icon FILE_MANAGER = IconLoader.getIcon("/icons/file_manager.png");
-    public static final Icon COINS = IconLoader.getIcon("/icons/coins_in_hand.png");
-    public static final Icon IMPORT = IconLoader.getIcon("/icons/disk.png");
-    public static final Icon EXPORT = IconLoader.getIcon("/icons/file_save.png");
-    public static final Icon COPY = IconLoader.getIcon("/icons/page_copy.png");
-    public static final Icon PASTE = IconLoader.getIcon("/icons/paste_plain.png");
-    public static final Icon CLEARALL = IconLoader.getIcon("/icons/cross.png");
-    public static final Icon PROTECT_OFF = IconLoader.getIcon("/icons/set_password.png");
-    public static final Icon PROTECT_ON = IconLoader.getIcon("/icons/set_passwordon.png");
+    public static final Icon CROSS = findIcon("/icons/cross.png");
+    public static final Icon CANCEL = findIcon("/icons/cancel.png");
+    public static final Icon TICK = findIcon("/icons/tick.png");
+    public static final Icon QUESTION = findIcon("/icons/question.png");
+    public static final Icon EXPANDALL = findIcon("/icons/toggle_expand.png");
+    public static final Icon COLLAPSEALL = findIcon("/icons/toggle.png");
+    public static final Icon SELECT = findIcon("/icons/select.png");
+    public static final Icon FILE_LINK_BIG = findIcon("/icons/file_link.png");
+    public static final Icon URL_LINK_BIG = findIcon("/icons/url_link.png");
+    public static final Icon FILE_MANAGER = findIcon("/icons/file_manager.png");
+    public static final Icon COINS = findIcon("/icons/coins_in_hand.png");
+    public static final Icon IMPORT = findIcon("/icons/disk.png");
+    public static final Icon EXPORT = findIcon("/icons/file_save.png");
+    public static final Icon COPY = findIcon("/icons/page_copy.png");
+    public static final Icon PASTE = findIcon("/icons/paste_plain.png");
+    public static final Icon CLEARALL = findIcon("/icons/cross.png");
+    public static final Icon PROTECT_OFF = findIcon("/icons/set_password.png");
+    public static final Icon PROTECT_ON = findIcon("/icons/set_passwordon.png");
   }
 
   public static class PopUp {
 
-    public static final Icon EDITTEXT = IconLoader.getIcon("/icons/text.png");
-    public static final Icon ADD = IconLoader.getIcon("/icons/add.png");
-    public static final Icon DELETE = IconLoader.getIcon("/icons/delete.png");
-    public static final Icon CLONE = IconLoader.getIcon("/icons/draw_clone.png");
-    public static final Icon NOTE = IconLoader.getIcon("/icons/note.png");
-    public static final Icon URL = IconLoader.getIcon("/icons/url.png");
-    public static final Icon TOPIC = IconLoader.getIcon("/icons/brick.png");
-    public static final Icon FILE = IconLoader.getIcon("/icons/disk.png");
-    public static final Icon EXPANDALL = IconLoader.getIcon("/icons/toggle_expand.png");
-    public static final Icon COLLAPSEALL = IconLoader.getIcon("/icons/toggle.png");
-    public static final Icon SHOWJUMPS = IconLoader.getIcon("/icons/showjumps.png");
-    public static final Icon OPTIONS = IconLoader.getIcon("/icons/settings.png");
-    public static final Icon INFO = IconLoader.getIcon("/icons/info.png");
-    public static final Icon COLORS = IconLoader.getIcon("/icons/color_swatches.png");
-    public static final Icon EXPORT = IconLoader.getIcon("/icons/document_export.png");
-    public static final Icon IMPORT = IconLoader.getIcon("/icons/document_import.png");
-    public static final Icon PAGE = IconLoader.getIcon("/icons/page.png");
-    public static final Icon PRINTER = IconLoader.getIcon("/icons/printer.png");
-    public static final Icon EMOTICONS = IconLoader.getIcon("/icons/emoticons.png");
-    public static final Icon IMAGES = IconLoader.getIcon("/icons/pictures.png");
-    public static final Icon TEXT_ALIGN = IconLoader.getIcon("/icons/text_align.png");
-    public static final Icon TEXT_ALIGN_LEFT = IconLoader.getIcon("/icons/text_align_left.png");
-    public static final Icon TEXT_ALIGN_CENTER = IconLoader.getIcon("/icons/text_align_center.png");
-    public static final Icon TEXT_ALIGN_RIGHT = IconLoader.getIcon("/icons/text_align_right.png");
+    public static final Icon EDITTEXT = findIcon("/icons/text.png");
+    public static final Icon ADD = findIcon("/icons/add.png");
+    public static final Icon DELETE = findIcon("/icons/delete.png");
+    public static final Icon CLONE = findIcon("/icons/draw_clone.png");
+    public static final Icon NOTE = findIcon("/icons/note.png");
+    public static final Icon URL = findIcon("/icons/url.png");
+    public static final Icon TOPIC = findIcon("/icons/brick.png");
+    public static final Icon FILE = findIcon("/icons/disk.png");
+    public static final Icon EXPANDALL = findIcon("/icons/toggle_expand.png");
+    public static final Icon COLLAPSEALL = findIcon("/icons/toggle.png");
+    public static final Icon SHOWJUMPS = findIcon("/icons/showjumps.png");
+    public static final Icon OPTIONS = findIcon("/icons/settings.png");
+    public static final Icon INFO = findIcon("/icons/info.png");
+    public static final Icon COLORS = findIcon("/icons/color_swatches.png");
+    public static final Icon EXPORT = findIcon("/icons/document_export.png");
+    public static final Icon IMPORT = findIcon("/icons/document_import.png");
+    public static final Icon PAGE = findIcon("/icons/page.png");
+    public static final Icon PRINTER = findIcon("/icons/printer.png");
+    public static final Icon EMOTICONS = findIcon("/icons/emoticons.png");
+    public static final Icon IMAGES = findIcon("/icons/pictures.png");
+    public static final Icon TEXT_ALIGN = findIcon("/icons/text_align.png");
+    public static final Icon TEXT_ALIGN_LEFT = findIcon("/icons/text_align_left.png");
+    public static final Icon TEXT_ALIGN_CENTER = findIcon("/icons/text_align_center.png");
+    public static final Icon TEXT_ALIGN_RIGHT = findIcon("/icons/text_align_right.png");
   }
 
 }
