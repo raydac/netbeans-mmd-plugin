4a050d4826a9918324bac5cad687e7462df39b76
Igor Maznitsa <rrg4400@gmail.com>
2022-08-31 01:25:02 +0300

    added start.cmd into windows

---
+++ mind-map/scia-reto/src/assembly/windows/start.cmd
@@ -0,0 +1,9 @@
+set "SCIARETO_HOME=%cd%"
+
+rem uncomment the line below if graphics works slowly
+rem set "JAVA_EXTRA_GFX_FLAGS=-Dsun.java2d.opengl=true"
+
+set "JAVA_FLAGS=-client -XX:+IgnoreUnrecognizedVMOptions -Xmx2G --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED -Dsun.java2d.dpiaware=true -Dswing.aatext=true -Dawt.useSystemAAFontSettings=on"
+set "JAVA_RUN=javaw.exe"
+
+"%SCIARETO_HOME%\jre\bin\%JAVA_RUN%" %JAVA_FLAGS% %JAVA_EXTRA_GFX_FLAGS% -jar "%SCIARETO_HOME%\scia-reto.jar" %*
