f73060373e01d90c7ba2eea91d67b47d318439f9
Igor Maznitsa <rrg4400@gmail.com>
2022-09-02 00:21:20 +0300

    default PLANTUML_LIMIT_SIZE set to 32768

--- mind-map/scia-reto/src/main/java/SciaReto.java
+++ mind-map/scia-reto/src/main/java/SciaReto.java
@@ -8,41 +8,50 @@ import javax.annotation.Nullable;
 
 public final class SciaReto {
 
-    private SciaReto() {
+  private static final String ENV_PLANTUML_LIMIT_SIZE = "PLANTUML_LIMIT_SIZE";
+
+  private SciaReto() {
+  }
+
+  /**
+   * Copy of similar method in UiUtils because it should be called before Swing activation.
+   *
+   * @return detected value as string, can be null
+   */
+  @Nullable
+  private static String readUiScaleFactor() {
+    String result = PreferencesManager.getInstance().getPreferences()
+        .get(SciaRetoStarter.PROPERTY_SCALE_GUI, null);
+    if (result != null && !result.matches("\\s*[1-5](?:\\.5)?\\s*")) {
+      result = null;
+    }
+    return result == null ? result : result.trim();
+  }
+
+  private static void processUiScale() {
+    final String guiScaleFactor = readUiScaleFactor();
+    if (guiScaleFactor != null) {
+      System.out.println("UI scale factor: " + guiScaleFactor);
+      System.setProperty("sun.java2d.uiScale.enabled", "true");
+      System.setProperty("sun.java2d.uiScale", guiScaleFactor);
+    } else {
+      System.out.println("UI scale factor is not set");
+    }
+  }
+
+  public static void main(@Nonnull @MustNotContainNull final String... args) {
+    if (System.getProperty(ENV_PLANTUML_LIMIT_SIZE) == null) {
+      System.setProperty(ENV_PLANTUML_LIMIT_SIZE, Integer.toString(32768));
     }
 
-    /**
-     * Copy of similar method in UiUtils because it should be called before Swing activation.
-     * @return detected value as string, can be null
-     */
-    @Nullable
-    private static String readUiScaleFactor() {
-        String result = PreferencesManager.getInstance().getPreferences().get(SciaRetoStarter.PROPERTY_SCALE_GUI, null);
-        if (result != null && !result.matches("\\s*[1-5](?:\\.5)?\\s*")) {
-            result = null;
-        }
-        return result == null ? result : result.trim();
+    final String presentedScale = System.getProperty("sun.java2d.uiScale", null);
+    if (presentedScale == null) {
+      processUiScale();
+    } else {
+      System.out.println(
+          "Detected presented sun.java2d.uiScale among System properties: " + presentedScale);
     }
 
-    private static void processUiScale(){
-        final String guiScaleFactor = readUiScaleFactor();
-        if (guiScaleFactor != null) {
-            System.out.println("UI scale factor: " + guiScaleFactor);
-            System.setProperty("sun.java2d.uiScale.enabled", "true");
-            System.setProperty("sun.java2d.uiScale", guiScaleFactor);
-        } else {
-            System.out.println("UI scale factor is not set");
-        }
-    }
-    
-    public static void main(@Nonnull @MustNotContainNull final String... args) {
-        final String presentedScale = System.getProperty("sun.java2d.uiScale", null);
-        if (presentedScale == null) {
-            processUiScale();
-        } else {
-            System.out.println("Detected presented sun.java2d.uiScale among System properties: " + presentedScale);
-        }
-
-        SciaRetoStarter.main(args);
-    }
+    SciaRetoStarter.main(args);
+  }
 }
