c6dfe383b48575591e08da6871e43a97b122ce50
Igor Maznitsa <rrg4400@gmail.com>
2024-01-14 15:18:23 +0200

    refactoring

--- mind-map/idea-mindmap/src/com/igormaznitsa/ideamindmap/view/KnowledgeViewPane.java
+++ mind-map/idea-mindmap/src/com/igormaznitsa/ideamindmap/view/KnowledgeViewPane.java
@@ -16,49 +16,25 @@
 
 package com.igormaznitsa.ideamindmap.view;
 
+import static java.util.Objects.requireNonNull;
+
 import com.igormaznitsa.ideamindmap.utils.AllIcons;
-import com.intellij.ide.DataManager;
-import com.intellij.ide.PsiCopyPasteManager;
+import com.intellij.ide.SelectInContext;
 import com.intellij.ide.SelectInTarget;
-import com.intellij.ide.dnd.aware.DnDAwareTree;
 import com.intellij.ide.projectView.impl.AbstractProjectViewPane;
-import com.intellij.ide.projectView.impl.ProjectViewTree;
-import com.intellij.ide.ui.customization.CustomizationUtil;
-import com.intellij.ide.util.treeView.AbstractTreeStructure;
-import com.intellij.openapi.actionSystem.ActionPlaces;
-import com.intellij.openapi.actionSystem.DataContext;
-import com.intellij.openapi.actionSystem.IdeActions;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.ActionCallback;
-import com.intellij.openapi.util.registry.Registry;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.ui.ScrollPaneFactory;
-import com.intellij.util.EditSourceOnDoubleClickHandler;
-import com.intellij.util.OpenSourceUtil;
-import com.intellij.util.ui.tree.TreeUtil;
-import java.awt.Font;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.util.MissingResourceException;
 import javax.annotation.Nonnull;
 import javax.swing.Icon;
 import javax.swing.JComponent;
-import javax.swing.JScrollPane;
-import javax.swing.JTree;
-import javax.swing.ToolTipManager;
-import javax.swing.event.TreeModelEvent;
-import javax.swing.event.TreeModelListener;
-import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.DefaultTreeModel;
-import javax.swing.tree.TreeModel;
-import javax.swing.tree.TreePath;
-import javax.swing.tree.TreeSelectionModel;
+import javax.swing.JLabel;
 import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
 
 public class KnowledgeViewPane extends AbstractProjectViewPane {
   @NonNls
   public static final String ID = "NBKnowledgePane";
-  private JScrollPane myComponent;
 
   public KnowledgeViewPane(@Nonnull Project project) {
     super(project);
@@ -71,7 +47,7 @@ public class KnowledgeViewPane extends AbstractProjectViewPane {
 
   @Override
   public Icon getIcon() {
-    return AllIcons.Logo.MINDMAP;
+    return requireNonNull(AllIcons.Logo.MINDMAP);
   }
 
   @Nonnull
@@ -81,140 +57,37 @@ public class KnowledgeViewPane extends AbstractProjectViewPane {
   }
 
   @Override
-  public JComponent createComponent() {
-    if (myComponent != null) {
-      return myComponent;
-    }
-
-    DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode(null);
-    DefaultTreeModel treeModel = new DefaultTreeModel(rootNode);
-    myTree = createTree(treeModel);
-    enableDnD();
-    myComponent = ScrollPaneFactory.createScrollPane(myTree);
-    myTreeStructure = createStructure();
-
-    installComparator();
-    initTree();
-
-    return myComponent;
-  }
-
-  private DnDAwareTree createTree(final TreeModel treeModel) {
-    return new ProjectViewTree(myProject, treeModel) {
-      @Override
-      public String toString() {
-        return getTitle() + " " + super.toString();
-      }
-
-      @Override
-      public void setFont(Font font) {
-        try {
-          if (Registry.is("bigger.font.in.project.view")) {
-            font = font.deriveFont(font.getSize() + 1.0f);
-          }
-          super.setFont(font);
-        } catch (MissingResourceException ex) {
-          super.setFont(font);
-        }
-      }
-    };
-  }
-
-  private AbstractTreeStructure createStructure() {
-    return new KnowledgeViewPanelTreeStructure(this.myProject);
-  }
-
-  private void initTree() {
-    myTree.getSelectionModel().setSelectionMode(TreeSelectionModel.DISCONTIGUOUS_TREE_SELECTION);
-    myTree.setRootVisible(false);
-    myTree.setShowsRootHandles(true);
-    myTree.expandPath(new TreePath(myTree.getModel().getRoot()));
-    myTree.setSelectionPath(new TreePath(myTree.getModel().getRoot()));
-
-    EditSourceOnDoubleClickHandler.install(myTree);
-
-    ToolTipManager.sharedInstance().registerComponent(myTree);
-    TreeUtil.installActions(myTree);
-
-    myTree.getSelectionModel().addTreeSelectionListener(e -> fireTreeChangeListener());
-    myTree.getModel().addTreeModelListener(new TreeModelListener() {
-      @Override
-      public void treeNodesChanged(TreeModelEvent e) {
-        fireTreeChangeListener();
-      }
-
-      @Override
-      public void treeNodesInserted(TreeModelEvent e) {
-        fireTreeChangeListener();
-      }
-
-      @Override
-      public void treeNodesRemoved(TreeModelEvent e) {
-        fireTreeChangeListener();
-      }
-
-      @Override
-      public void treeStructureChanged(TreeModelEvent e) {
-        fireTreeChangeListener();
-      }
-    });
-
-    new SpeedSearch(myTree);
-
-    myTree.addKeyListener(new KeyAdapter() {
-      @Override
-      public void keyPressed(KeyEvent e) {
-        if (KeyEvent.VK_ENTER == e.getKeyCode()) {
-
-          final DefaultMutableTreeNode selectedNode = ((ProjectViewTree) myTree).getSelectedNode();
-          if (selectedNode != null && !selectedNode.isLeaf()) {
-            return;
-          }
-
-          DataContext dataContext = DataManager.getInstance().getDataContext(myTree);
-          OpenSourceUtil.openSourcesFrom(dataContext, false);
-        } else if (KeyEvent.VK_ESCAPE == e.getKeyCode()) {
-          if (e.isConsumed()) {
-            return;
-          }
-          PsiCopyPasteManager copyPasteManager = PsiCopyPasteManager.getInstance();
-          boolean[] isCopied = new boolean[1];
-          if (copyPasteManager.getElements(isCopied) != null && !isCopied[0]) {
-            copyPasteManager.clear();
-            e.consume();
-          }
-        }
-      }
-    });
-    CustomizationUtil.installPopupHandler(myTree, IdeActions.GROUP_PROJECT_VIEW_POPUP, ActionPlaces.PROJECT_VIEW_POPUP);
-  }
-
-  @Nonnull
-  @Override
-  public ActionCallback updateFromRoot(boolean restoreExpandedPaths) {
-    return ActionCallback.DONE;
-  }
-
-  @Override
-  public void select(Object element, VirtualFile file, boolean requestFocus) {
-    if (file != null) {
-
-    }
-  }
-
-  @Override
   public int getWeight() {
     return 51175;
   }
 
   @Override
-  public JTree getTree() {
-    return this.myTree;
+  public JComponent createComponent() {
+    return new JLabel("Not implemented yet");
   }
 
   @Override
-  public SelectInTarget createSelectInTarget() {
-    return new SelectInTargetImpl(this.myProject);
+  public ActionCallback updateFromRoot(boolean b) {
+    return ActionCallback.DONE;
   }
 
+  @Override
+  public void select(Object o, VirtualFile virtualFile, boolean b) {
+
+  }
+
+  @Override
+  public @NotNull SelectInTarget createSelectInTarget() {
+    return new SelectInTarget() {
+      @Override
+      public boolean canSelect(SelectInContext selectInContext) {
+        return false;
+      }
+
+      @Override
+      public void selectIn(SelectInContext selectInContext, boolean b) {
+
+      }
+    };
+  }
 }
