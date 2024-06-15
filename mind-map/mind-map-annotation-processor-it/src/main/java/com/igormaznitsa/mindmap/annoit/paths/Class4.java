18d635de3eb8772b314351b96bffe2653fb11475
Igor Maznitsa <rrg4400@gmail.com>
2024-06-15 21:43:08 +0300

    improved test

--- mind-map/mind-map-annotation-processor-it/src/main/java/com/igormaznitsa/mindmap/annoit/paths/Class4.java
+++ mind-map/mind-map-annotation-processor-it/src/main/java/com/igormaznitsa/mindmap/annoit/paths/Class4.java
@@ -2,14 +2,17 @@ package com.igormaznitsa.mindmap.annoit.paths;
 
 import com.igormaznitsa.mindmap.annotations.MmdFileRef;
 import com.igormaznitsa.mindmap.annotations.MmdTopic;
+import java.util.List;
+import java.util.Objects;
 
 @MmdFileRef(target = RootFile.class)
-@MmdTopic(title = "multi-path1", path = "goose")
+@Deprecated
+@SuppressWarnings("unchecked")
+@MmdTopic(title = "multi-root", path = "goose", anchor = true)
 public class Class4 {
 
   @MmdTopic(title = "multi-path1", path = "goose")
   public void class4method1() {
-
   }
 
   @MmdTopic(title = "multi-path1", path = {"goose"})
@@ -20,6 +23,13 @@ public class Class4 {
   @MmdTopic(title = "multi-path1", path = {"goose"})
   public void class4method3() {
 
+  }
+
+  @Deprecated
+  @MmdTopic(title = "between", path = "goose")
+  @SafeVarargs
+  public final void class4method1(List<Class4>... args) {
+    Objects.requireNonNull(args);
   }
 
 }
--- mind-map/mind-map-annotation-processor-it/src/test/java/com/igormaznitsa/mindmap/annoit/PathTest.java
+++ mind-map/mind-map-annotation-processor-it/src/test/java/com/igormaznitsa/mindmap/annoit/PathTest.java
@@ -3,17 +3,11 @@ package com.igormaznitsa.mindmap.annoit;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
-import com.igormaznitsa.mindmap.annotations.MmdTopic;
 import com.igormaznitsa.mindmap.model.Extra;
 import com.igormaznitsa.mindmap.model.ExtraFile;
-import com.igormaznitsa.mindmap.model.MMapURI;
 import com.igormaznitsa.mindmap.model.MindMap;
 import com.igormaznitsa.mindmap.model.Topic;
-import java.io.File;
-import java.nio.file.Files;
-import java.util.List;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -40,32 +34,29 @@ public class PathTest extends AbstractMmdTest {
     assertNotNull(pathTopic.getChildren().get(0).getExtras().get(Extra.ExtraType.FILE));
 
     final Topic gooseTopic = findForPath(map, "root", "goose");
-    assertEquals(4, gooseTopic.getChildren().size());
+    assertEquals(5, gooseTopic.getChildren().size());
     assertTrue(gooseTopic.getChildren().stream()
         .allMatch(x -> x.getExtras().get(Extra.ExtraType.FILE) != null));
   }
 
   @Test
   @Ignore("because not fixed yet")
-  public void testFileAnchorPointsMmdTopic() throws Exception {
-    final File baseDir = this.getBaseDir().toFile().getParentFile();
+  public void testFileAnchorOnAnnotationBetweenAnotherAnnotations() throws Exception {
     final MindMap map =
         this.loadMindMap("com/igormaznitsa/mindmap/annoit/paths/RootFile.mmd");
-    map.stream()
-        .filter(t -> t.getExtras().containsKey(Extra.ExtraType.FILE))
-        .forEach(t -> {
-          ExtraFile extraFile = (ExtraFile)t.getExtras().get(Extra.ExtraType.FILE);
-          final MMapURI uri = extraFile.getAsURI();
-          final int lineNumber = Integer.parseInt(uri.getParameters().getProperty("line"));
-          final List<String> lines;
-          try {
-            lines = Files.readAllLines(uri.asFile(baseDir).toPath());
-          } catch (Exception ex) {
-            throw new IllegalStateException("Can't read " + uri, ex);
-          }
-          final String srcLine = lines.get(lineNumber - 1);
-          Assert.assertTrue("uri: " + uri +" line: " + srcLine,srcLine.contains("@"+ MmdTopic.class.getSimpleName()));
-        });
+    final Topic topic = findForPath(map, "root", "goose", "between");
+    final ExtraFile extraFile = (ExtraFile) topic.getExtras().get(Extra.ExtraType.FILE);
+    Assert.assertEquals("27", extraFile.getAsURI().getParameters().getProperty("line"));
+  }
+
+  @Test
+  @Ignore("because not fixed yet")
+  public void testFileAnchorPointsMmdTopicBeforeClass() throws Exception {
+    final MindMap map =
+        this.loadMindMap("com/igormaznitsa/mindmap/annoit/paths/RootFile.mmd");
+    final Topic topic = findForPath(map, "root", "goose", "multi-root");
+    final ExtraFile extraFile = (ExtraFile) topic.getExtras().get(Extra.ExtraType.FILE);
+    Assert.assertEquals("11", extraFile.getAsURI().getParameters().getProperty("line"));
   }
 
 }
