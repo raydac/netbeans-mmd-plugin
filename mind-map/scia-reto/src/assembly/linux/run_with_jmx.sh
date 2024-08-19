defcc155397cb8cfbd98d4e4274f6ad1c562db86
Igor Maznitsa <rrg4400@gmail.com>
2024-08-19 13:18:35 +0300

    added JMX starter

---
+++ mind-map/scia-reto/src/assembly/linux/run_with_jmx.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SCIARETO_HOME="$(dirname ${BASH_SOURCE[0]})"
+SR_JAVA_HOME=$SCIARETO_HOME/jre
+
+# uncomment the line below if graphics works slowly
+# JAVA_EXTRA_GFX_FLAGS="-Dsun.java2d.opengl=true"
+
+JAVA_FLAGS="-XX:+IgnoreUnrecognizedVMOptions -Xmx2G -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=8009 -Dcom.sun.management.jmxremote.rmi.port=8009 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED -Dsun.java2d.dpiaware=true -Dswing.aatext=true -Dawt.useSystemAAFontSettings=on"
+JAVA_LOG_CONFIG=$SCIARETO_HOME/logger.properties
+JAVA_RUN=$SR_JAVA_HOME/bin/java
+
+$JAVA_RUN $JAVA_FLAGS $JAVA_EXTRA_GFX_FLAGS "-Djava.util.logging.config.file=$JAVA_LOG_CONFIG" -jar $SCIARETO_HOME/scia-reto.jar $@
